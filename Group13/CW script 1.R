9802e6f8f06e5dfbb9c97b2cc42b1c8e76f0d295
Satrand <sara.strand0221@gmail.com>
2025-11-11 17:54:53 +0000

    Updated script for combining csv files

--- Group13/CW script 1.R
+++ Group13/CW script 1.R
@@ -6,7 +6,12 @@ files <- list.files(data_folder, full.names = TRUE, pattern = "\\.csv$")
 
 # Step 1: Read the first file to get column headers
 ref_raw <- fread(files[1], header = FALSE)
-column_names <- ref_raw$V1   # assign the field names as column headers
+
+#capitalize all names in the first column 
+ref_raw[, V1 := toupper(V1)]
+
+# assign the field names as column headers
+column_names <- ref_raw$V1   
 
 # Transpose the first (reference) file's values to create reference row
 # V1 = col 1 (headings), V2 = col 2
@@ -15,30 +20,20 @@ colnames(ref_t) <- column_names
 
 #step 2: Write the first row to the output CSV
 output_file <- "/Users/hayashireiko/Desktop/combined_data.csv"
-fwrite(final_data, output_file)
+fwrite(ref_t, output_file)
 
 # Step 3: Loop through the remaining files and append to CSV
 for (i in 2:length(files)) {
-  dt <- fread(files[i], header = FALSE)
-  t_dt <- transpose(dt[, .(V2)])
+  dt <- fread(files[i], header = FALSE) #removes headers
+  dt[, V1 := toupper(V1)] #capitalize first column
+  dt <- dt[match(column_names, V1)] #sort by the reference order
+  t_dt <- transpose(dt[, .(V2)])  
+  colnames(t_dt) <- column_names
   
   # Append to CSV without rewriting the header
   fwrite(t_dt, output_file, append = TRUE)
 }
 
-# vec = rep(0, length(files))
-# for (i in length(files)) {
-#   t_dt <- transpose(dt[, 1]) # get header row
-#   print(t_dt)
-#   vec[i] = t_dt
-# }
-# unique(vec)
-# print(vec)
 
 
 
-
-
-
-
-  
